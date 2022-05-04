23dca97c0aeea64b114cbf82b079ea8a529b31eb
Adam Januszewski <Adpjan@att.net>
2022-05-04 10:07:59 -0400

    Initial Commit

---
+++ FruitSelector/FruitSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSelectorApp.swift
+//  FruitSelector
+//
+//  Created by adam janusewski on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
